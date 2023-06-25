b90d9bbd3f014f839f4f814c74d9eaae688dfd78
jotapemoraez <jotapemoraez@gmail.com>
2023-06-24 23:27:43 -0300

    add host log

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(8080);
+  
+  const server = await app.listen(8080, () => {
+    const address = server.address();
+    const host = address.address;
+    const port = address.port;
+
+    console.log(`Application is running on: http://${host}:${port}`);
+  });
 }
 bootstrap();
