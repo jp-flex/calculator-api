5f70212df2b36726d59d9538598b243f1927aa70
jotapemoraez <jotapemoraez@gmail.com>
2023-06-24 22:36:13 -0300

    change

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Docker if not already installed
-apt-get update
-apt-get install -y docker.io
-
-# Build and run the Docker containers
-cd /var/www/calculator_api
-docker-compose up -d
