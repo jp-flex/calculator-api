caf672ee0e585e0dc47bb7ec0a48420fcf8aae1f
jotapemoraez <jotapemoraez@gmail.com>
2023-06-25 01:16:00 -0300

    fix env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,7 @@ import { HttpModule } from '@nestjs/axios';
 import { OperationSeeder } from './seed/operation.seeder';
 import { CalculationModule } from './calculation/calculation.module';
 import { RecordController } from './controllers/record.controller';
-import * as dotenv from 'dotenv';
-dotenv.config();
-console.log( process.env.PGHOST)
+
 @Module({
   imports: [HttpModule,
     TypeOrmModule.forRoot({
